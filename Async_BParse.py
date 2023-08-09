8f5a378aeccf407a03124c3f7102492100fa95c9
IcelandT <1185330343@qq.com>
2023-08-09 16:33:51 +0800

    删除不必要代码

--- Async_BParse.py
+++ Async_BParse.py
@@ -1,4 +1,4 @@
-import sys
+from sys import exit
 import re
 import json
 import os
@@ -24,10 +24,10 @@ class Requests(object):
 
     async def get(self, session: ClientSession) -> str:
         async with session.get(
-            url=self.url,
-            headers=self.headers,
-            params=self.params,
-            allow_redirects=self.allow_redirects
+                url=self.url,
+                headers=self.headers,
+                params=self.params,
+                allow_redirects=self.allow_redirects
         ) as response:
             return await response.text()
 
@@ -68,13 +68,13 @@ class Requests(object):
 class BDown(object):
     headers = {
         'Referer': 'https://www.bilibili.com/',
+        'Cookie': 'SESSDATA=6fa654bd%2C1705137814%2C77ce0%2A721XIprtlSi35wkAFvu9Hr7ZTZ0Y_q2QTtYva2zkwkCOpSLv10zMuVL_S8l2HZNXlpU2-XEwAAXQA',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
     }
 
-    def __init__(self, bvid, works) -> None:
-        self.bvid = bvid
-        self.works = works
-        self.task = asyncio.Queue()
+    def __init__(self, vid) -> None:
+        self.vid = vid
+        self.video_dir = '.\\video'
 
     @staticmethod
     def parse_audio_frequency(response) -> tuple[str, str, str]:
@@ -93,26 +93,16 @@ class BDown(object):
             json_script_code = json.loads(script_code[0])
             data = json_script_code.get('data')
             dash = data.get('dash')
-            video_url = dash.get('video')[0].get('baseUrl')
-            audio_url = dash.get('audio')[0].get('baseUrl')
-            video_title = video_title_list[0]
-            if video_url and audio_url:
-                return video_url, audio_url, video_title
-            else:
-                console = Console()
-                console.print('提取时出现错误', style='bold red')
-                sys.exit(1)
-
-    async def create_task(self) -> None:
-        if isinstance(self.bvid, list):
-            for vid in self.bvid:
-                await self.task.put(vid)
-        elif isinstance(self.bvid, str):
-            await self.task.put(self.bvid)
-        else:
-            console = Console()
-            console.print('仅允许输入列表或字符串', style='bold red')
-            sys.exit(1)
+            if dash:
+                video_url = dash.get('video')[0].get('baseUrl')
+                audio_url = dash.get('audio')[0].get('baseUrl')
+                video_title = video_title_list[0]
+                if video_url and audio_url:
+                    return video_url, audio_url, video_title
+                else:
+                    console = Console()
+                    console.print('提取时出现错误', style='bold red')
+                    exit(1)
 
     async def download(self, url, url_type, video_title) -> None:
         if url_type == 'video':
@@ -122,9 +112,9 @@ class BDown(object):
 
         async with ClientSession() as session:
             async with session.get(url, headers=self.headers) as response:
-                if not os.path.exists('.\\video'):
-                    os.mkdir('.\\video')
-                save_path = os.path.join('.\\video', file_name)
+                if not os.path.exists(self.video_dir):
+                    os.mkdir(self.video_dir)
+                save_path = os.path.join(self.video_dir, file_name)
                 # 文件总大小
                 total_size = int(response.headers['Content-Length'])
                 # 设置进度条样式和参数
@@ -144,7 +134,7 @@ class BDown(object):
                         progress_bar.update(len(chunk))
                     progress_bar.close()
 
-    async def crawl(self, bvid) -> None:
+    async def parser(self, bvid) -> None:
         url = 'https://www.bilibili.com/video/' + bvid + '/'
         params = {
             'spm_id_from': '333.1007.tianma.2-3-6.click',
@@ -165,14 +155,19 @@ class BDown(object):
         await self.download(audio_url, 'audio', video_title)
 
         # 合并音视频
-        video_file = os.path.join('.\\video', video_title + '_video.m4s')
-        audio_file = os.path.join('.\\video', video_title + '_audio.m4s')
+        video_file = os.path.join(self.video_dir, video_title + '_video.m4s')
+        audio_file = os.path.join(self.video_dir, video_title + '_audio.m4s')
         video = VideoFileClip(video_file)
         audio = AudioFileClip(audio_file)
         video = video.set_audio(audio)
         # 输出合并后的文件
-        out_file = os.path.join('.\\video', video_title + '.mp4')
-        video.write_videofile(out_file, codec='libx264', audio_codec='aac')
+        out_file = os.path.join(self.video_dir, video_title + '.mp4')
+        video.write_videofile(
+            out_file,
+            codec='libx264',
+            audio_codec='aac',
+            fps=30.303
+        )
         console = Console()
         console.print(':victory_hand_medium-light_skin_tone:'
                       ' 下载成功! '
@@ -181,31 +176,21 @@ class BDown(object):
         os.remove(video_file)
         os.remove(audio_file)
 
-    async def worker(self) -> None:
-        if not self.task.empty():
-            bvid = await self.task.get()
-            await self.crawl(bvid)
-            self.task.task_done()
-        else:
-            pass
-
     async def start(self) -> None:
-        await self.create_task()
-        while not self.task.empty():
-            workers = [
-                asyncio.create_task(self.worker())
-                for _ in range(int(self.works))
-            ]
-            await asyncio.gather(*workers)
+        tasks = [
+            asyncio.create_task(
+                self.parser(self.vid)
+            )
+        ]
+        await asyncio.gather(*tasks)
 
 
-async def main(bvid, works) -> None:
-    spider = BDown(bvid, works)
+async def main(vid) -> None:
+    spider = BDown(vid)
     await spider.start()
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(
         description='Bilibili parse',
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -216,17 +201,20 @@ if __name__ == '__main__':
                   / __  |/ // /    / // __  |/ // /    / /   / /_/ / /| | / /_/ /\__ \/ __/   
                  / /_/ // // /____/ // /_/ // // /____/ /   / ____/ ___ |/ _, _/___/ / /___   
                 /_____/___/_____/___/_____/___/_____/___/  /_/   /_/  |_/_/ |_|/____/_____/   
-                                                                                                                                                                                                                                                
+
                 如何使用？
-                方法: python async_b_down.py -b 视频的bvid号 -w 并发数量
-                例如: python async_b_down.py -b BV1UQ4y1d7os -w 1
+                方法: python ABParse.py -b 视频的bvid号
+                例如: python ABParse.py -b BV1UQ4y1d7os
             """
         ))
-    parser.add_argument('-b', '--bvid', type=str, required=True, help='bvid号')
-    parser.add_argument('-w', '--works', type=int, required=True, help='并发数量')
-    try:
-        args = parser.parse_args()
-        asyncio.run(main(args.bvid, args.works))
-    except:
+    parser.add_argument('-v', '--vid', type=str, help='vid号')
+    args = parser.parse_args()
+    vid = args.vid
+    if vid:
+        asyncio.run(main(vid))
+    else:
         console = Console()
-        console.print(':light_bulb: 未填写对应的参数 :light_bulb:', style='bold yellow')
+        console.print(
+            ':warning-emoji:[bold red] 未填写对应的参数!\n'
+            ':warning-emoji:[bold red] 输入 python ABParse.py -h 获取帮助'
+        )

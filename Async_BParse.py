da95948c3572cec970f349f09b9d7f39fba194d6
IcelandT <1185330343@qq.com>
2023-08-10 17:43:39 +0800

    加入视频基本信息显示

--- Async_BParse.py
+++ Async_BParse.py
@@ -1,16 +1,20 @@
 from sys import exit
-import re
-import json
+from re import findall
+from json import loads
 import os
-import argparse
-import textwrap
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from textwrap import dedent
 
 import asyncio
 import aiofiles
 from tqdm import tqdm
 from rich.console import Console
+from rich.table import Table
 from aiohttp import ClientSession
 from moviepy.editor import VideoFileClip, AudioFileClip
+
+
+console = Console()
 
 
 class Requests(object):
@@ -79,28 +83,41 @@ class BDown(object):
     @staticmethod
     def parse_audio_frequency(response) -> tuple[str, str, str]:
         """
-        解析 response 中的 video url 和 audio url
+        解析视频中的信息，如 video url、播放量等
         :param response:
         :return:
         """
-        # 匹配 script、和视频标题
-        script_code_template = '<script>window.__playinfo__=(.*?)</script>'
-        video_title_template = '<title data-vue-meta="true">(.*?)</title>'
+        match_script_code = '<script>window.__playinfo__=(.*?)</script>'
+        match_video_title = '<title data-vue-meta="true">(.*?)</title>'
+        match_play_volume = '视频播放量 (.*?)、'
+        match_barrage_quantity = '弹幕量 (.*?)、'
 
-        script_code = re.findall(script_code_template, response)
-        video_title_list = re.findall(video_title_template, response)
-        if script_code and video_title_list:
-            json_script_code = json.loads(script_code[0])
-            data = json_script_code.get('data')
-            dash = data.get('dash')
+        script_code_list = findall(match_script_code, response)
+        video_title_list = findall(match_video_title, response)
+        play_volume_list = findall(match_play_volume, response)
+        barrage_quantity_list = findall(match_barrage_quantity, response)
+        if script_code_list and video_title_list:
+            json_script_code = loads(script_code_list[0])
+            dash = json_script_code.get('data').get('dash')
             if dash:
                 video_url = dash.get('video')[0].get('baseUrl')
                 audio_url = dash.get('audio')[0].get('baseUrl')
                 video_title = video_title_list[0]
                 if video_url and audio_url:
+                    if play_volume_list or barrage_quantity_list:
+                        # 视频基本信息
+                        table = Table(show_header=True, header_style='bold yellow')
+                        table.add_column('播放量')
+                        table.add_column('弹幕量')
+                        table.add_column('标题')
+                        table.add_row(
+                            play_volume_list[0],
+                            barrage_quantity_list[0],
+                            video_title
+                        )
+                        console.print(table)
                     return video_url, audio_url, video_title
                 else:
-                    console = Console()
                     console.print('提取时出现错误', style='bold red')
                     exit(1)
 
@@ -123,7 +140,7 @@ class BDown(object):
                     unit='B',
                     unit_scale=True,
                     dynamic_ncols=True,
-                    ncols=30,
+                    ncols=30
                 )
                 progress_bar.set_description(url_type)
                 async with aiofiles.open(save_path, mode='wb') as f:
@@ -166,9 +183,9 @@ class BDown(object):
             out_file,
             codec='libx264',
             audio_codec='aac',
-            fps=30.303
+            fps=30.303,
+            preset='fast'
         )
-        console = Console()
         console.print(':victory_hand_medium-light_skin_tone:'
                       ' 下载成功! '
                       ':victory_hand_medium-light_skin_tone:', style='bold cyan')
@@ -177,12 +194,19 @@ class BDown(object):
         os.remove(audio_file)
 
     async def start(self) -> None:
-        tasks = [
-            asyncio.create_task(
-                self.parser(self.vid)
+        self.vid = self.vid.replace(' ', '')
+        vid_list = self.vid.split(',')
+        if len(vid_list) == 1:
+            tasks = [
+                asyncio.create_task(self.parser(vid))
+                for vid in vid_list
+            ]
+            await asyncio.gather(*tasks)
+        else:
+            console.print(
+                ':warning-emoji: 当前仅允许传入一个vid!\n'
+                ':warning-emoji: 输入 python Async_BParse.py -h 获取帮助'
             )
-        ]
-        await asyncio.gather(*tasks)
 
 
 async def main(vid) -> None:
@@ -191,10 +215,10 @@ async def main(vid) -> None:
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
+    parser = ArgumentParser(
         description='Bilibili parse',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent(
+        formatter_class=RawDescriptionHelpFormatter,
+        epilog=dedent(
             r"""
                     ____  ______    ________  ______    ____   ____  ___    ____  _____ ______
                    / __ )/  _/ /   /  _/ __ )/  _/ /   /  _/  / __ \/   |  / __ \/ ___// ____/
@@ -213,8 +237,7 @@ if __name__ == '__main__':
     if vid:
         asyncio.run(main(vid))
     else:
-        console = Console()
         console.print(
             ':warning-emoji:[bold red] 未填写对应的参数!\n'
-            ':warning-emoji:[bold red] 输入 python ABParse.py -h 获取帮助'
+            ':warning-emoji:[bold red] 输入 python Async_BParse.py -h 获取帮助'
         )
